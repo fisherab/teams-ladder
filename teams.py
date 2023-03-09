af84490fb5a7bbf65d3247b00a3be146869828c7
Steve Fisher <dr.s.m.fisher@gmail.com>
2023-03-09 11:43:34 +0000

    First go

---
+++ teams.py
@@ -0,0 +1,29 @@
+import csv
+
+with open('Friday Ladder - Sheet1.csv', newline='') as csvfile:
+    results = {}
+    for row in csv.DictReader(csvfile):
+        pairA = row['Pair A']
+        pairB = row['Pair B']
+        score = 4 - int(row['Position'])
+        if pairA not in results: results[pairA] = []
+        results[pairA].append(score)
+        if pairB not in results: results[pairB] = []
+        results[pairB].append(score)
+
+totals = {}
+for name, scores in results.items():
+    points = sorted(scores,reverse=True)[:6]
+    totals[name] = sum(points)
+
+scores = sorted(totals.items(), key=lambda item: item[1], reverse=True)
+with open('Friday Ladder - Sheet2.csv', newline='', mode='w') as csvfile:
+    fieldnames = ["Name", "Points", "Individual"]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for name, score in scores:
+        individual = sorted(results[name],reverse=True)    
+        print (name, score, individual)
+        writer.writerow({'Name': name, 'Points': score, 'Individual': individual})
+    
+    
